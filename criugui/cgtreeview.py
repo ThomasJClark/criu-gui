c4696099be3da1a96928c1103840013cd39caf26
Tom Clark <tclark@redhat.com>
2015-07-31 09:45:29 -0400

    Removed unused import

--- criugui/cgtreeview.py
+++ criugui/cgtreeview.py
@@ -15,7 +15,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from gi.repository import Gdk
 from gi.repository import Gtk
 from gi.repository import Pango
 
