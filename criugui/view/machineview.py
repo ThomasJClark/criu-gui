6f6269af727917a2f857226de8583b81fd5d785c
Tom Clark <tclark@redhat.com>
2015-08-04 10:17:38 -0400

    Allow removing machines

--- criugui/view/machineview.py
+++ criugui/view/machineview.py
@@ -31,7 +31,7 @@ class MachineView(Gtk.Notebook):
 
         self.machine = machine
 
-        self.header = Gtk.Label()
+        self.label = Gtk.Label()
         self.treeview = CGTreeView()
 
         scrolledwin = Gtk.ScrolledWindow()
@@ -41,30 +41,50 @@ class MachineView(Gtk.Notebook):
         sep = Gtk.Separator()
         sep.set_orientation(Gtk.Orientation.VERTICAL)
 
+        closebutton = Gtk.Button.new_from_icon_name("window-close-symbolic", Gtk.IconSize.BUTTON)
+        closebutton.connect("clicked", self.remove_machine)
+
+        header = Gtk.HBox()
+        header.pack_start(self.label, True, True, 0)
+        header.pack_start(closebutton, True, False, 5)
+        header.show_all()
+
         self.grid = Gtk.Grid()
         self.grid.attach(scrolledwin, 0, 0, 1, 1)
         self.grid.attach(sep, 1, 0, 1, 2)
-        self.append_page(self.grid, self.header)
+        self.append_page(self.grid, header)
 
         self.update()
+
+    def remove_machine(self, *_):
+        """Remove this machine from the list of connected machines and close the SSH connection."""
+
+        if self.machine.ssh_client:
+            self.machine.ssh_client.close()
+
+        self.destroy()
 
     def update(self):
         """Update the view with the latest data in self.machine."""
 
-        self.header.set_markup("<b>%s</b>" % GLib.markup_escape_text(self.machine.hostname))
+        self.label.set_markup("<b>%s</b>" % GLib.markup_escape_text(self.machine.hostname))
 
         # If an error occured while loading the process data, add an infobar containing the error
         # message.
         if self.machine.error_text:
-            def close_infobar(infobar, response):
-                infobar.destroy()
+            def on_infobar_response(infobar, response):
+                if response == Gtk.ResponseType.CLOSE:
+                    infobar.destroy()
+                elif response == Gtk.ResponseType.REJECT:
+                    self.remove_machine()
 
             infobar = self.grid.get_child_at(0, 1)
 
             if infobar is None:
                 infobar = Gtk.InfoBar(message_type=Gtk.MessageType.ERROR, show_close_button=True)
+                infobar.add_button("Remove", Gtk.ResponseType.REJECT)
                 infobar.get_content_area().add(Gtk.Label())
-                infobar.connect("response", close_infobar)
+                infobar.connect("response", on_infobar_response)
                 self.grid.attach(infobar, 0, 1, 1, 1)
 
             infobar.get_content_area().get_children()[0].set_text(self.machine.error_text)
